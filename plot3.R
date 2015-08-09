de84ca27189ad8febc5caa227530b823a5070678
Stephan Kurpjuweit <stephan.kurpjuweit@gmail.com>
2015-08-09 20:33:29 +0200

    Legend corrected

--- plot3.R
+++ plot3.R
@@ -28,7 +28,7 @@ lines(df_sub$DateTime, df_sub[[names[2]]], type = "l", col = colors[2])
 lines(df_sub$DateTime, df_sub[[names[3]]], type = "l", col = colors[3])
 
 # print legend, adjust text parameters so that png export looks ok
-legend("topright", lty = 1, col = colors, legend = names, pt.cex = 1, cex = 0.85, text.width = 50000)
+legend("topright", lty = 1, col = colors, legend = names, pt.cex = 1, cex = 0.65, text.width = 50000)
 
 # save as PNG file
 dev.copy(png, file = "Plot3.png")
